decc7e291296f8d1b325670e82d2eb2a650e69ff
DEll pc <bushramjad16@gmail.com>
2020-04-18 02:56:41 +0500

    Api call added for testing

---
+++ app/src/main/java/com/example/pharmease/models/DefaultResponse.kt
@@ -0,0 +1,4 @@
+package com.example.pharmease.models
+
+data class DefaultResponse(var error : Boolean, var message :String) {
+}
--- app/src/main/java/com/example/pharmease/signup/SignupSecond.kt
+++ app/src/main/java/com/example/pharmease/signup/SignupSecond.kt
@@ -1,30 +1,27 @@
 package com.example.pharmease.signup
 
 import android.os.Bundle
-import android.os.Handler
-import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
-import androidx.navigation.findNavController
-import androidx.navigation.fragment.findNavController
 import com.example.pharmease.R
+import com.example.pharmease.api.RetrofitClient
+import com.example.pharmease.models.DefaultResponse
 import kotlinx.android.synthetic.main.signup_2.*
+import retrofit2.Call
+import retrofit2.Callback
+import retrofit2.Response
 
-/**
- * A simple [Fragment] subclass.
- */
 class SignupSecond : Fragment() {
 
     private val SPLASH_TIME_OUT:Long = 1000 // 1 sec
 
-
     override fun onCreateView(inflater: LayoutInflater, container: ViewGroup?, savedInstanceState: Bundle?
     ): View? {
         (activity as AppCompatActivity?)!!.supportActionBar!!.show()
-
         val root = inflater.inflate(R.layout.signup_2, container, false)
         return root
     }
@@ -34,15 +31,38 @@ class SignupSecond : Fragment() {
 
         progressBar4.visibility = View.GONE
 
-            signup2.setOnClickListener() {
+        signup2.setOnClickListener() {
 
-                progressBar4.visibility = View.VISIBLE
+            progressBar4.visibility = View.VISIBLE
+//            Handler().postDelayed({
+//                findNavController().navigate(R.id.action_nav_signup2_to_nav_enable_location_notification)
+//            }, SPLASH_TIME_OUT)
 
-                Handler().postDelayed({
-                    findNavController().navigate(R.id.action_nav_signup2_to_nav_enable_location_notification)
-                }, SPLASH_TIME_OUT)
 
+            val fname = fname.text.toString().trim()
+            val lname = lname.text.toString().trim()
+            val phone = phone.text.toString().trim()
 
+//            if(fname.isEmpty()){
+//                fname.error = "First name Required"
+//                fname.requestFocus()
+//                return@setOnClickListener
+//            }
+
+            RetrofitClient.instance.createUser("email", "name", "password", "school")
+                ?.enqueue(object: Callback<DefaultResponse?>{
+
+                    override fun onFailure(call: Call<DefaultResponse?>, t: Throwable) {
+                        Toast.makeText(activity,t.message,Toast.LENGTH_SHORT).show()
+                    }
+
+                    override fun onResponse(call: Call<DefaultResponse?>, response: Response<DefaultResponse?>) {
+                        Toast.makeText(activity,response.body()?.message,Toast.LENGTH_SHORT).show()
+                    }
+
+                })
+
+            //findNavController().navigate(R.id.action_nav_signup2_to_nav_enable_location_notification)
 
         }
     }
